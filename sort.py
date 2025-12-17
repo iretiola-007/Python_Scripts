ec2aefe6fdca14ef9f941f74e6fddba6431def61
Iretiola <abisigairetiola@gmail.com>
2025-12-17 17:38:37 +0100

    Create sort.py

---
+++ sort.py
@@ -0,0 +1,17 @@
+# list of objects
+listt = ["cable", "book", "pencil", "adapter"]
+print(listt)
+
+length = len(listt) # store the length of the elements in a variable
+
+# define a sort() function to print sorted list
+def sort():
+    for i in range(length):
+        for j in range(0, length - i - 1):
+            if listt[j] > listt[j + 1]:
+                listt[j], listt[j + 1] = listt[j + 1], listt[j]
+    print(listt)
+
+sort() # call the function
+
+# the original list is printed before the sorted list
