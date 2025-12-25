353f8578186523e19d07a24555dd5459dcd0c1b5
Iretiola <abisigairetiola@gmail.com>
2025-12-25 21:36:53 +0100

    Update module.py

--- module.py
+++ module.py
@@ -1,11 +1,22 @@
+# a simple module
+
+# define the class (blueprint)
 class Greet:
     def __init__(self, name):
         self.name = name
+    
+    # define a method in your blueprint
     def msg(self ):
         print(f"Hello, {self.name}!")
+
+# get user input
+name = input("What's your name? ")
     
-Greet("Iretiola").msg()
+# create an object and call the function
+Greet(name).msg()
+
+# alternatively you can store the object in a variable like this👇🏻
+welcome_message = Greet(name)
+welcome_message.msg()
 
 
-# a simple module
-# further notes later 
