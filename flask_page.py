38219b39b89ce03461db5ffa752be6398778c780
Iretiola <abisigairetiola@gmail.com>
2025-12-26 23:26:16 +0100

    Update flask_page.py

--- flask_page.py
+++ flask_page.py
@@ -13,3 +13,5 @@ if __name__ == "__main__":
 # run "python app.py" on your terminal and visit the url
 
 # this will start a local web server and show your message when you open the URL that shows up in your terminal (http://127.0.0.1:5000)
+
+# note: replace "app.py" in the command: "python app.py" with your actual file name
