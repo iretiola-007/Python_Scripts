134adfe46faf27f95a182c104390a42ec92f4136
Iretiola <abisigairetiola@gmail.com>
2025-12-26 23:24:48 +0100

    Create flask_page.py

---
+++ flask_page.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask world!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# running flask
+# run "python app.py" on your terminal and visit the url
+
+# this will start a local web server and show your message when you open the URL that shows up in your terminal (http://127.0.0.1:5000)
