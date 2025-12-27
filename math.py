f8645a23632c0781611aa5d50b07381536ef3dd0
Iretiola <abisigairetiola@gmail.com>
2025-12-27 23:36:22 +0100

    Create math.py

---
+++ math.py
@@ -0,0 +1,8 @@
+total_seconds = 7835
+hours = total_seconds // 3600
+remaining_seconds = total_seconds % 3600
+minutes = remaining_seconds // 60
+seconds = remaining_seconds % 60
+print(hours, minutes, seconds)
+
+# comments come in later
